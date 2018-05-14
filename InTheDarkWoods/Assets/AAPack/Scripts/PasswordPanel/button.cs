21cf8341e4fac9ebf3780237334db2cd031648c9
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2018-05-14 11:23:48 +0200

    commit ToDo

---
+++ InTheDarkWoods/Assets/AAPack/Scripts/PasswordPanel/button.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour, IInteractive {
+
+    [SerializeField]
+    TextMesh text;
+
+    [SerializeField]
+    char znak;
+
+    [SerializeField]
+    bool isEnter = false;
+
+    [SerializeField]
+    string kod;
+
+    [SerializeField]
+    Door door;
+
+
+    public void OnAction(GameObject player)
+    {
+        if (!isEnter)
+        {
+            if (text.text.Length < 4)
+            {
+                text.text = text.text + znak;
+            }
+            else
+            {
+                text.text = "" + znak;
+            }
+        }
+        else
+        {
+            if (text.text.Equals(kod))
+            {
+                door.OpenDoor(true);
+            }
+            else
+            {
+                text.text = "";
+            }
+        }
+    }
+
+    public PlayerCursor.Cursor OnFocused()
+    {
+        return PlayerCursor.Cursor.hand;
+    }
+
+}
