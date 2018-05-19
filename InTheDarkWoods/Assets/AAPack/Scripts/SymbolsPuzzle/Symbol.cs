ece7792da9535ccd01150e5441647c932c0c0a7c
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2018-05-20 01:31:57 +0200

    Added symbols puzzle
    +SymbolsPuzzle.cs
    +Symbol.cs

---
+++ InTheDarkWoods/Assets/AAPack/Scripts/SymbolsPuzzle/Symbol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Symbol : MonoBehaviour , IInteractive {
+
+    [SerializeField]
+    SymbolsPuzzle puzzle;
+
+    [SerializeField]
+    Image symbol;
+
+    public PlayerCursor.Cursor OnFocused()
+    {
+        return PlayerCursor.Cursor.hand;
+    }
+
+    public void OnAction(GameObject player)
+    {
+        symbol.enabled = !symbol.enabled;
+        puzzle.Check();
+    }
+
+    public bool Check()
+    {
+        return symbol.enabled;
+    }
+
+}
---
+++ InTheDarkWoods/Assets/AAPack/Scripts/SymbolsPuzzle/SymbolsPuzzle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class SymbolsPuzzle : MonoBehaviour {
+
+    [SerializeField]
+    Symbol[] symbolsOn;
+
+    [SerializeField]
+    Symbol[] symbolsOff;
+
+    private bool flag;
+
+    public void Check()
+    {
+        flag = true;
+        foreach (Symbol s in symbolsOn)
+        {
+            if(!s.Check()) flag = false;
+        }
+        foreach (Symbol s in symbolsOff)
+        {
+            if (s.Check()) flag = false;
+        }
+        if (flag)
+        {
+            Debug.Log("Open");
+        }
+    }
+
+
+}
