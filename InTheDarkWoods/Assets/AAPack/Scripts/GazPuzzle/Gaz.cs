770333dcc340c5ff7325ef1c7031fe6dca044f0c
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2018-06-22 11:07:39 +0200

    added gasPuzzle

---
+++ InTheDarkWoods/Assets/AAPack/Scripts/GazPuzzle/Gaz.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaz : MonoBehaviour {
+
+    [SerializeField]
+    ParticleSystem gas;
+
+    private bool isOn;
+
+    public void OnOff(bool f)
+    {
+        isOn = f;
+        if (f)
+        {
+            gas.Play();
+        }
+        else
+        {
+            gas.Stop();
+            gas.Clear();
+        }
+    }
+
+    public bool GetIsOn()
+    {
+        return isOn;
+    }
+
+    private void Start()
+    {
+        isOn = true;
+        gas.Play();
+    }
+
+}
