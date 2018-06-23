e99e800b83b46d0a6c3627be2ef56ce43f408cf4
Sofiia Bilyk <bilykalfavitska.sophia@gmail.com>
2018-06-23 22:42:41 +0200

    commit

--- InTheDarkWoods/Assets/MainMenu.cs
+++ InTheDarkWoods/Assets/MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour {
 	{
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 	}
+
+	public void Exit()
+	{
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
 }
