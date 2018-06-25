6151d80fd30ecffd1d080528ee350d373d0d7429
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2018-06-25 13:19:24 +0200

    Added subtitles code

---
+++ InTheDarkWoods/Assets/AAPack/Scripts/TextTriger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTriger : MonoBehaviour {
+
+    [SerializeField]
+    Text text;
+
+    [SerializeField]
+    float time;
+
+    [SerializeField]
+    bool repeat;
+
+    private void Start()
+    {
+        text.enabled = false;    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Sleep());
+            if (!repeat)
+            {
+                DestroyObject(this);
+            }
+        }
+    }
+
+    IEnumerator Sleep()
+    {
+        text.enabled = true;
+        yield return new WaitForSecondsRealtime(time);
+        text.enabled = false;
+        
+    }
+
+}
